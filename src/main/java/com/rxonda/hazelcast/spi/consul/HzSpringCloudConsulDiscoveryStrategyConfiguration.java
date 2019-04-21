a5fc2216e59f2a483bcbc370874988a648e33168
Raphael R. Costa <rxonda@gmail.com>
2019-04-20 22:55:33 -0300

    Creating Bean DiscoveryStrategyConfig to consume the properties

--- src/main/java/com/rxonda/hazelcast/spi/consul/HzSpringCloudConsulDiscoveryStrategyConfiguration.java
+++ src/main/java/com/rxonda/hazelcast/spi/consul/HzSpringCloudConsulDiscoveryStrategyConfiguration.java
@@ -1,5 +1,7 @@
 package com.rxonda.hazelcast.spi.consul;
 
+import com.hazelcast.config.DiscoveryStrategyConfig;
+import com.hazelcast.spi.discovery.DiscoveryStrategyFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -9,6 +11,10 @@ import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.client.serviceregistry.ServiceRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @ConditionalOnBean(value = {DiscoveryClient.class, ServiceRegistry.class})
@@ -21,9 +27,20 @@ public class HzSpringCloudConsulDiscoveryStrategyConfiguration {
     @Autowired
     private ServiceRegistry<Registration> serviceRegistry;
 
-    @Bean(name = "hzDiscoveryStrategyFactory")
+    @Bean(name = "discoveryStrategyFactory")
     @ConditionalOnMissingBean
-    public HzSpringCloudConsulDiscoveryStrategyFactory hzDiscoveryStrategyFactory() {
+    public DiscoveryStrategyFactory hzDiscoveryStrategyFactory() {
         return new HzSpringCloudConsulDiscoveryStrategyFactory(discoveryClient, serviceRegistry);
+    }
+
+    @Bean(name = "discoveryStrategyConfig")
+    public DiscoveryStrategyConfig hzDiscoveryStrategyConfig(Environment env, DiscoveryStrategyFactory discoveryStrategyFactory) {
+        Map<String, Comparable> props = new HashMap<>();
+        props.put("consul.name", env.getProperty("hazelcast.discovery.consul.props.group-name"));
+        props.put("consul.tags", env.getProperty("hazelcast.discovery.consul.props.tags"));
+        props.put("consul.discovery-delay", env.getProperty("hazelcast.discovery.consul.props.discovery-delay","10000"));
+        props.put("consul.check-interval", env.getProperty("hazelcast.discovery.consul.props.check-interval","30s"));
+        props.put("consul.prefer-public", env.getProperty("hazelcast.discovery.consul.props.prefer-public", "false"));
+        return  new DiscoveryStrategyConfig(discoveryStrategyFactory, props);
     }
 }
