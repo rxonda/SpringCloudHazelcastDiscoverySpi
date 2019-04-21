a8bdd3addd845b188b3b147072ffc101107e1308
Raphael R. Costa <rxonda@gmail.com>
2019-04-21 00:45:45 -0300

    Removing generic definition that was preventing the bean to be identified

--- src/main/java/com/rxonda/hazelcast/spi/consul/HzSpringCloudConsulDiscoveryStrategyAutoConfiguration.java
+++ src/main/java/com/rxonda/hazelcast/spi/consul/HzSpringCloudConsulDiscoveryStrategyAutoConfiguration.java
@@ -3,12 +3,15 @@ package com.rxonda.hazelcast.spi.consul;
 import com.hazelcast.config.DiscoveryStrategyConfig;
 import com.hazelcast.spi.discovery.DiscoveryStrategyFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.client.serviceregistry.ServiceRegistry;
+import org.springframework.cloud.consul.discovery.ConsulDiscoveryClientConfiguration;
+import org.springframework.cloud.consul.serviceregistry.ConsulServiceRegistryAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -17,15 +20,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@AutoConfigureAfter(value = { ConsulServiceRegistryAutoConfiguration.class, ConsulDiscoveryClientConfiguration.class })
 @ConditionalOnBean(value = {DiscoveryClient.class, ServiceRegistry.class})
 @ConditionalOnProperty(value = "spring.cloud.consul.enabled")
-public class HzSpringCloudConsulDiscoveryStrategyConfiguration {
+public class HzSpringCloudConsulDiscoveryStrategyAutoConfiguration {
 
     @Autowired
     private DiscoveryClient discoveryClient;
 
     @Autowired
-    private ServiceRegistry<Registration> serviceRegistry;
+    private ServiceRegistry serviceRegistry;
 
     @Bean(name = "discoveryStrategyFactory")
     @ConditionalOnMissingBean
@@ -36,11 +40,11 @@ public class HzSpringCloudConsulDiscoveryStrategyConfiguration {
     @Bean(name = "discoveryStrategyConfig")
     public DiscoveryStrategyConfig hzDiscoveryStrategyConfig(Environment env, DiscoveryStrategyFactory discoveryStrategyFactory) {
         Map<String, Comparable> props = new HashMap<>();
-        props.put("consul.name", env.getProperty("hazelcast.discovery.consul.props.group-name"));
-        props.put("consul.tags", env.getProperty("hazelcast.discovery.consul.props.tags"));
-        props.put("consul.discovery-delay", env.getProperty("hazelcast.discovery.consul.props.discovery-delay","10000"));
-        props.put("consul.check-interval", env.getProperty("hazelcast.discovery.consul.props.check-interval","30s"));
-        props.put("consul.prefer-public", env.getProperty("hazelcast.discovery.consul.props.prefer-public", "false"));
+        props.put("consul.name", env.getProperty("hazelcast.discovery.consul.group-name"));
+        props.put("consul.tags", env.getProperty("hazelcast.discovery.consul.tags"));
+        props.put("consul.discovery-delay", env.getProperty("hazelcast.discovery.consul.discovery-delay","10000"));
+        props.put("consul.check-interval", env.getProperty("hazelcast.discovery.consul.check-interval","30s"));
+        props.put("consul.prefer-public", env.getProperty("hazelcast.discovery.consul.prefer-public", "false"));
         return  new DiscoveryStrategyConfig(discoveryStrategyFactory, props);
     }
 }
